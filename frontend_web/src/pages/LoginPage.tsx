604cc65870977f0010021c22252bd093e2be2c36
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-04 19:04:41 +0700

    init login page

---
+++ frontend_web/src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import './page.css'
+
+const LoginPage = () => {
+    return (
+        <>
+            <div className='h-svh flex flex-col gap-4 md:gap-12 items-center justify-center bg-teal-400'>
+                <p className='text-white font-extrabold text-lg md:text-3xl'>LOGIN ADMIN</p>
+                <div className='w-4/5 h-4/7 md:w-2/7 md:h-3/5 bg-white'>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginPage;
