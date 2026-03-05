a8b8c4bdd5f5453ce98272c0b4ac960fe7733723
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-05 22:56:40 +0700

    cleaning login page

--- frontend_web/src/pages/LoginPage.tsx
+++ frontend_web/src/pages/LoginPage.tsx
@@ -1,13 +1,10 @@
 import './page.css'
+import LoginRoot from '../features/login/components/LoginRoot';
 
 const LoginPage = () => {
     return (
         <>
-            <div className='h-svh flex flex-col gap-4 md:gap-12 items-center justify-center bg-teal-400'>
-                <p className='text-white font-extrabold text-lg md:text-3xl'>LOGIN ADMIN</p>
-                <div className='w-4/5 h-4/7 md:w-2/7 md:h-3/5 bg-white'>
-                </div>
-            </div>
+            <LoginRoot/>
         </>
     )
 }
