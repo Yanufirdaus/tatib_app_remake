78403d425ceb9e70847578da057c54aae80924cb
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-05 22:55:31 +0700

    creating auth store

---
+++ frontend_web/src/store/auth.store.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand"
+
+type User = {
+  id: number
+  role: string
+}
+
+type AuthState = {
+  user: User | null
+  isAuthenticated: boolean
+  setUser: (user: User) => void
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+
+  setUser: (user) =>
+    set({
+      user,
+      isAuthenticated: true
+    }),
+
+  logout: () =>
+    set({
+      user: null,
+      isAuthenticated: false
+    })
+}))
