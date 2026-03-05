0b7fe3f754e72bf4008b14c06df3ca9ad772d218
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-05 23:03:46 +0700

    adding specific origin for cors midleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors())
+app.use(cors(
+  {
+    origin:"http://localhost:5173",
+    credentials:true
+  }
+))
 
 app.use(authRouter);
 app.use(kelasRouter);
@@ -25,6 +30,6 @@ app.use(pelanggaranRouter)
 app.use(catatanPelanggaranRouter);
 app.use(semesterRouter);
 
-app.listen(3001, () => {
+app.listen(3001, "0.0.0.0", () => {
   console.log('server running on port 3001')
 })
