2ba4a078012280baee47eb37725a3b5df934f96c
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-05 22:55:54 +0700

    creating api client

---
+++ frontend_web/src/services/apiClient.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../config/api';
+
+export const apiClient = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.error("API Error:", error.response?.data || error.message);
+        return Promise.reject(error);
+    }
+)
