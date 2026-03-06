98a085c33c5974ea6fc281f61e215778060ddb6c
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-06 22:32:26 +0700

    adding logout handler at header

--- frontend_web/src/layouts/components/Header.tsx
+++ frontend_web/src/layouts/components/Header.tsx
@@ -1,6 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/auth.store";
+import { logout } from "../../features/auth/services/auth.services";
+
 
 const Header = () => {
+  const { user, logoutStore } = useAuthStore()
+  const navigate = useNavigate()
+
+  console.log("user:",user?.id)
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      logoutStore();
+      navigate("/", { replace: true });
+    }
+  };
 
   return (
     <header className="w-full h-16 bg-teal-400 flex items-center justify-between px-6">
@@ -10,14 +28,27 @@ const Header = () => {
           Tatib Admin
         </h1>
       </div>
-      <div className="flex items-center gap-4">
-            <Link 
-                to="/login" 
-                className="text-sm md:text-base text-white hover:text-gray-200"
-            >
-                Login Admin
-            </Link>
-      </div>
+
+      {user ? (
+        <div className="flex items-center gap-4">
+              <button
+                  className="text-sm md:text-base text-white hover:text-gray-200"
+                  onClick={handleLogout}
+              >
+                  Logout
+              </button>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+              <Link 
+                  to="/login" 
+                  className="text-sm md:text-base text-white hover:text-gray-200"
+              >
+                  Login Admin
+              </Link>
+        </div>
+      )}
+      
     </header>
   );
 };
