69f31e3104c85f8a3f25981ef5aae62bf3a00fce
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-06 22:49:06 +0700

    adding fetch api for getMe and logout endpoint

--- frontend_web/src/features/auth/services/auth.services.ts
+++ frontend_web/src/features/auth/services/auth.services.ts
@@ -13,3 +13,22 @@ export const login = async (data: LoginFormValues) => {
         throw new Error(error.response?.data?.message || "Login gagal")
     }
 }
+
+export const getMe = async () => {
+    try {
+        const response = await apiClient.get("/me");
+        return response.data;
+    } catch (error:any) {
+        throw new Error(error.response?.data?.message || "gagal mendapatkan data user")
+    }
+  
+};
+
+export const logout = async () => {
+    try {
+        const response = await apiClient.post("logout");
+        return response.data;
+    } catch (error:any) {
+        throw new Error(error.response?.data?.message || "Logout gagal")
+    }
+}
