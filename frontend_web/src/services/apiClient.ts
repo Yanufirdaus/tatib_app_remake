15b3394fc0b62df6be4258ef37eac7c2f064ad36
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-06 22:28:26 +0700

    adding refresh token request in the api client interceptor

--- frontend_web/src/services/apiClient.ts
+++ frontend_web/src/services/apiClient.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../config/api';
+import { useAuthStore } from '../store/auth.store';
+import { useNavigate } from 'react-router-dom';
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
@@ -11,8 +13,40 @@ export const apiClient = axios.create({
 
 apiClient.interceptors.response.use(
     (response) => response,
-    (error) => {
+    async (error) => {
         console.error("API Error:", error.response?.data || error.message);
+        const originalRequest = error.config
+        const requestUrl = originalRequest.url
+        console.log("url:",requestUrl)
+
+        if (
+            error.response?.status === 401 && 
+            !originalRequest._retry
+        ) {
+            originalRequest._retry = true
+
+            try {
+                await apiClient.post(
+                    "/refresh-token", {}, {withCredentials: true}
+                )
+                console.log("original request: ", originalRequest)
+                
+                const response = await apiClient(originalRequest);
+
+                
+                const { logoutStore } = useAuthStore()
+                const navigate = useNavigate()
+
+                if (requestUrl === "logout") {
+                    logoutStore();
+                    navigate("/", { replace: true });
+                }
+
+                return response;
+            } catch {
+                useAuthStore.getState().logoutStore()
+            }
+        }
         return Promise.reject(error);
     }
 )
