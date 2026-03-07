e62948819d7ce055d08c074aa1a40ae892292549
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-07 22:12:59 +0700

    create hero section for home admin

---
+++ frontend_web/src/features/home_admin/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <div className="w-full justify-items-center items-center py-4 md:py-8">
+        <h1 className="text-center text-lg md:text-2xl w-3/4 md:w-full font-semibold md:font-bold">
+            Selamat Datang di Dashboard Admin
+        </h1>
+    </div>
+  );
+};
+
+export default HeroSection;
