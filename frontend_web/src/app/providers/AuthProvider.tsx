aa4efc159c44a94a0a7a6297f7523b6663906303
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-07 22:14:48 +0700

    create auth provider to upgraded auth handling

---
+++ frontend_web/src/app/providers/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import { useEffect, type ReactNode } from "react"
+import { useMe } from "../../features/auth/hooks/useMe"
+import { useAuthStore } from "../../store/auth.store"
+
+type Props = {
+  children: ReactNode;
+};
+
+
+const AuthProvider = ({ children }: Props) => {
+  const setUser = useAuthStore((s) => s.setUser)
+  const setAuthChecked = useAuthStore((s) => s.setAuthChecked)
+  const { data, isLoading } = useMe()
+
+  useEffect(() => {
+    if (data) {
+      setUser({
+        id: data.id,
+        role: data.role,
+      })
+    }
+  }, [data])
+
+  if (!isLoading) {
+    setAuthChecked()
+  }
+
+  return <>{children}</>
+//   return <>{children}</>
+}
+
+export default AuthProvider;
