bc47d511d6b0e2f5af888bbe52b6419ab6350448
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-07 22:04:38 +0700

    add authChecked condition at authstate

--- frontend_web/src/store/auth.store.ts
+++ frontend_web/src/store/auth.store.ts
@@ -8,18 +8,27 @@ type User = {
 type AuthState = {
   user: User | null
   isAuthenticated: boolean
+  authChecked: boolean
+
   setUser: (user: User) => void
+  setAuthChecked: () => void
   logoutStore: () => void
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   isAuthenticated: false,
+  authChecked: false,
 
   setUser: (user) =>
     set({
       user,
       isAuthenticated: true
+    }),
+
+  setAuthChecked: () =>
+    set({
+      authChecked: true
     }),
 
   logoutStore: () =>
