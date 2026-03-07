af5c57346b1361acb1e7951d5c8e78abc8769da7
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-07 22:04:59 +0700

    fix api interceptor

--- frontend_web/src/services/apiClient.ts
+++ frontend_web/src/services/apiClient.ts
@@ -11,42 +11,38 @@ export const apiClient = axios.create({
     }
 })
 
+
 apiClient.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        console.error("API Error:", error.response?.data || error.message);
-        const originalRequest = error.config
-        const requestUrl = originalRequest.url
-        console.log("url:",requestUrl)
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const requestUrl = originalRequest?.url;
 
-        if (
-            error.response?.status === 401 && 
-            !originalRequest._retry
-        ) {
-            originalRequest._retry = true
+    console.error("API Error:", error.response?.data || error.message);
 
-            try {
-                await apiClient.post(
-                    "/refresh-token", {}, {withCredentials: true}
-                )
-                console.log("original request: ", originalRequest)
-                
-                const response = await apiClient(originalRequest);
-
-                
-                const { logoutStore } = useAuthStore()
-                const navigate = useNavigate()
-
-                if (requestUrl === "logout") {
-                    logoutStore();
-                    navigate("/", { replace: true });
-                }
-
-                return response;
-            } catch {
-                useAuthStore.getState().logoutStore()
-            }
-        }
-        return Promise.reject(error);
+    if (requestUrl === "/refresh-token") {
+      useAuthStore.getState().logoutStore();
+      return Promise.reject(error);
     }
-)
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        await apiClient.post("/refresh-token", {}, { withCredentials: true });
+
+        return apiClient(originalRequest);
+
+      } catch (refreshError) {
+        useAuthStore.getState().logoutStore();
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
