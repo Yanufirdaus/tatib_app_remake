144dfe57d5ddaacf33181102a934620b56b5200b
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-07 22:10:18 +0700

    fix authchecking at rootlayout

--- frontend_web/src/layouts/RootLayout.tsx
+++ frontend_web/src/layouts/RootLayout.tsx
@@ -2,58 +2,39 @@ import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { useAuthStore } from "../store/auth.store";
-import { useMe } from "../features/auth/hooks/useMe";
-import { useEffect } from "react";
+import { ThreeDots } from 'react-loader-spinner';
 
 const RootLayout = () => {
-  const Location = useLocation();
-  const isLoginPage = Location.pathname === "/login";
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
 
-  const { user, setUser } = useAuthStore()
-
-  const { data, isLoading } = useMe()
-  
-  console.log(data)
-
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (isLoading) return
-    if (data && !user) {
-      setUser({ id: data.id, role: data.role })
-    }
-
-    if (user) {
-      if (location.pathname === "/" || location.pathname === "/login") {
-        navigate("/home", { replace: true })
-      }
-      return
-    }
-
-    if (!user) {
-      if (location.pathname === "/home") {
-        navigate("/", { replace: true })
-      }
-    }
-
-  }, [user, data, isLoading, location.pathname])
+  const authChecked = useAuthStore((s) => s.authChecked)
   
   return (
     <div className="min-h-screen w-full bg-gray-100 flex justify-center">
       
-      <div className="w-full max-w-screen-2xl min-h-screen bg-white">
+      <div className="flex flex-col min-h-screen w-full max-w-screen-2xl bg-white">
         {!isLoginPage && (
             <Header />
         )}
         
         <main className="flex-1">
             {/* <Outlet /> */}
-            {isLoading ? (
-            <div className="flex flex-1 h-svh items-center justify-center">
-                Loading...
+            {!authChecked ? (
+              <div className="flex flex-1 h-svh items-center justify-center">
+                  <ThreeDots
+                    visible={true}
+                    height="80"
+                    width="80"
+                    color="#2dd4bf"
+                    radius="9"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                  />
               </div>
             ) : (
-              <div className="flex-1">
+              <div>
                 <Outlet />
               </div>
             )}
