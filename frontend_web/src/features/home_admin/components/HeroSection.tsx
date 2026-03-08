7fcf67f5342513a896914d6c38b4d98b0719c004
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-08 23:44:09 +0700

    fix layout for admin homepage hero section

--- frontend_web/src/features/home_admin/components/HeroSection.tsx
+++ frontend_web/src/features/home_admin/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <div className="w-full justify-items-center items-center py-4 md:py-8">
+    <div className="w-full justify-items-center items-center ">
         <h1 className="text-center text-lg md:text-2xl w-3/4 md:w-full font-semibold md:font-bold">
             Selamat Datang di Dashboard Admin
         </h1>
