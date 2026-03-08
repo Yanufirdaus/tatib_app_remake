baae5c02278011fb511aa32bf4709dd968870ca2
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-08 23:39:30 +0700

    create add kelas zod schema

---
+++ frontend_web/src/features/login/schema/add.kelas.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const AddManyKelasSchema = z.object({kelas: z.array(z.object({
+    name: z.string().min(1, { message: "Nama kelas is required" }),
+    grade: z.coerce
+        .number()
+        .refine(val => [10, 11, 12].includes(val), {
+          message: "Grade must be 10, 11, or 12"
+        })
+}))});
+
+export type AddKelasFormValues = z.infer<typeof AddManyKelasSchema>
