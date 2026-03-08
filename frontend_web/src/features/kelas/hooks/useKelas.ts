72cd5a2caed68b4abe6729a1f193530ec2e45611
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-08 23:40:26 +0700

    create react query for kelas service

---
+++ frontend_web/src/features/kelas/hooks/useKelas.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { addKelas, deleteKelas, getKelas } from "../services/kelas.services"
+
+export const useKelas = () => {
+    return useQuery ({
+        queryKey: ["kelas"],
+        queryFn: getKelas,
+        staleTime: 5*60*1000
+    })
+}
+
+export const useDeleteKelas = () => {
+    const queryClient = useQueryClient();
+    
+    return useMutation({
+        mutationFn: deleteKelas,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["kelas"] })
+        }
+    })
+}
+
+export const useAddKelas = () => {
+    const queryClient = useQueryClient();
+    
+    return useMutation({
+        mutationFn: addKelas,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["kelas"] })
+        }
+    })
+}
