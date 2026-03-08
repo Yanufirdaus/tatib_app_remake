134dfb0c2ba9bfecc659ee91cefac600475d019f
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-08 23:42:25 +0700

    add hook for edit semester service

--- frontend_web/src/features/home_admin/hooks/useSemester.ts
+++ frontend_web/src/features/home_admin/hooks/useSemester.ts
@@ -1,10 +1,21 @@
-import { useQuery } from "@tanstack/react-query"
-import { getSemester } from "../services/semester.service"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { getSemester, updateSemester } from "../services/semester.service"
 
 export const useSemester = () => {
     return useQuery ({
         queryKey: ["semester"],
         queryFn: getSemester,
         staleTime: 5*60*1000
+    })
+}
+
+export const useEditSemester = () => {
+    const queryClient = useQueryClient();
+    
+    return useMutation({
+        mutationFn: updateSemester,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["semester"] })
+        }
     })
 }
