d7ac17c606ed81d5b30ff8ece8cf569213da99d8
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-08 23:37:26 +0700

    creating manajemen kelas page

---
+++ frontend_web/src/pages/ManajemenKelas.tsx
@@ -0,0 +1,11 @@
+import ManajemenKelasRoot from "../features/kelas/components/ManajemenKelasRoot";
+
+const ManajemenKelas = () => {
+    return (
+        <>
+            <ManajemenKelasRoot />
+        </>
+    )
+}
+
+export default ManajemenKelas;
