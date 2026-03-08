852c1f0ba4be0efecdaa1a6b37f748fd77199408
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-08 23:41:59 +0700

    add update semester at semester service

--- frontend_web/src/features/home_admin/services/semester.service.ts
+++ frontend_web/src/features/home_admin/services/semester.service.ts
@@ -1,4 +1,5 @@
 import { apiClient } from "../../../services/apiClient";
+import type { UpdateSemesterFormValues } from "../../login/schema/update.semester.schema";
 
 export const getSemester = async () => {
     try {
@@ -8,3 +9,12 @@ export const getSemester = async () => {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data semester")
     }
 };
+
+export const updateSemester = async (data: UpdateSemesterFormValues) => {
+    try {
+        const response = await apiClient.put("/current", data);
+        return response.data;
+    } catch (error:any) {
+        throw new Error(error.response?.data?.message || "gagal mendapatkan data semester")
+    }
+}
