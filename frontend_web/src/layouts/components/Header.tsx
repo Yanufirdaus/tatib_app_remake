c80837e06e5928ce60269ff2f9c372bb9c7d7fb6
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-09 21:56:17 +0700

    adding navigation while click title text at header

--- frontend_web/src/layouts/components/Header.tsx
+++ frontend_web/src/layouts/components/Header.tsx
@@ -28,9 +28,15 @@ const Header = () => {
     <header className="w-full h-12 md:h-16 bg-teal-400 flex items-center justify-between px-6">
         
       <div className="flex items-center gap-3">
-        <h1 className="text-lg md:text-xl text-white font-semibold">
-          Tatib Admin
-        </h1>
+        <a onClick={
+          () => {
+            user ? navigate("/home") : navigate("/")
+          }
+        }>
+          <h1 className="text-lg md:text-xl text-white font-semibold">
+            Tatib Admin
+          </h1>
+        </a>
       </div>
 
       {user ? (
