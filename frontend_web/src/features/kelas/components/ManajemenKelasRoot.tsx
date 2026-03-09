15f0b2df434747b37b17a2f92c0192fd5af48bbd
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-09 21:58:21 +0700

    add logic for add kelas on manajemen kelas route

--- frontend_web/src/features/kelas/components/ManajemenKelasRoot.tsx
+++ frontend_web/src/features/kelas/components/ManajemenKelasRoot.tsx
@@ -1,9 +1,48 @@
 import { FaPlusSquare } from "react-icons/fa"
 import ListKelas from "./ListKelas"
-import { useState } from "react"
+import { useFieldArray, useForm } from "react-hook-form"
+import { AddManyKelasSchema, type AddKelasFormValues } from "../../login/schema/add.kelas.schema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useAddKelas } from "../hooks/useKelas"
 
 const ManajemenKelasRoot = () => {
-    const [ addKelasCount, setAddKelasCount]  = useState(0)
+    const { mutate, isPending, error: addKelasError } = useAddKelas();
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+        reset
+    } = useForm<AddKelasFormValues>({
+        resolver: zodResolver(AddManyKelasSchema),
+        defaultValues: { kelas: [] } // array root
+    });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "kelas"
+    });
+
+    const onSubmit = (data: AddKelasFormValues) => {
+        console.log(data)
+        mutate(data, {
+            onSuccess: () => {
+                reset({
+                    kelas: []
+                })
+                alert("Kelas berhasil ditambahkan")
+
+                console.log(addKelasError?.message)
+            },
+            onError: (error:any) => {
+                const message = error?.response?.data?.message || error?.message || "Terjadi kesalahan";
+                alert(message);
+            }
+        }
+        )
+    }
+
     return (
          <div className="flex flex-col min-w-screen min-h-screen items-center py-8 gap-6">
             <div className="flex flex-row min-w-screen">
@@ -12,10 +51,17 @@ const ManajemenKelasRoot = () => {
                     <h1 className="text-lg md:text-2xl font-bold text-center">Manajemen Kelas</h1>
                 </div>
                 <div className="flex flex-row basis-2/7 justify-center items-center">
-                    <FaPlusSquare className="size-6 fill-green-600" onClick={() => setAddKelasCount(prev => prev + 1)}/>
+                    <FaPlusSquare 
+                        className="size-6 fill-green-600"
+                        onClick={() => {
+                            append({
+                                name: "",
+                                grade: "10"
+                            })
+                        }}/>
                 </div>
             </div>
-            <ListKelas addKelasCount={addKelasCount} cancelAddHandler={() => setAddKelasCount(0)}/>
+            <ListKelas fields={fields} register={register} cancelAddHandler={() => { remove() } } onSubmit={handleSubmit(onSubmit)} isPendingAddKelas={isPending} errors={errors}/>
         </div>
     )
 }
