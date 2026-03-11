1844b9c1b0174e052278eb1c3adabbead9439d58
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-11 19:13:58 +0700

    create getalljenispelanggaran service

--- backend/services/PelanggaranService.ts
+++ backend/services/PelanggaranService.ts
@@ -3,18 +3,31 @@ import { prisma } from "../lib/prisma";
 export class PelanggaranService {
     static async addPelanggaran( pelanggaran: AddPelanggaranDTO[] ) {
         const newPelanggaran = await prisma.pelanggaran.createMany({
-                data: pelanggaran.map(item => ({
+            data: pelanggaran.map(item => ({
                 jenisId: item.jenisId,
                 pelanggaran: item.pelanggaran,
                 poin: item.poin,
+                nomor: item.nomor
             })),
         });
         return newPelanggaran;
     }
 
+    static async getAllJenisPelanggaran() {
+        const jenisPelanggaran = await prisma.jenisPelanggaran.findMany({
+            orderBy: [
+                {id: "asc"},
+            ]
+        });
+        return jenisPelanggaran;
+    }
+
     static async getPelanggaranByJenisId(jenisId: number) {
         const pelanggaran = await prisma.pelanggaran.findMany({
             where: { jenisId: jenisId },
+            orderBy: [
+                {nomor: "asc"}
+            ]
         });
         return pelanggaran;
     }
