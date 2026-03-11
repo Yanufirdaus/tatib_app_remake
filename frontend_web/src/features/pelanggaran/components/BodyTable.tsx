6c2569d8813711b0a89335397be56b254464f845
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-11 19:03:39 +0700

    create component for bodytable

---
+++ frontend_web/src/features/pelanggaran/components/BodyTable.tsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import Input from "../../../components/ui/Input";
+import { FaEdit, FaSave, FaSpinner, FaTrashAlt } from "react-icons/fa";
+import type { EditPelanggaranComponentProps } from "../../../types/input.types";
+import { useDeletePelanggaran } from "../hooks/usePelanggaran";
+
+const BodyTable = (
+    {
+        pelanggaran,
+        editId,
+        register,
+        setEditId,
+        isPending,
+        setValue,
+        errors
+    } : EditPelanggaranComponentProps) => {
+    const [deleteId, setDeleteId] = useState<number | null>(null);
+
+    
+    
+    const { mutate: deletePelanggaran, isPending: isPendingDeletePelanggaran } = useDeletePelanggaran();
+
+    const handleDeletePelanggaraan = (id: number) => {
+        setDeleteId(id)
+        if (confirm("Yakin ingin menghapus kelas ini?")) {
+            deletePelanggaran(id, {
+                onError: (error:any) => {
+                    const message = error?.response?.data?.message || error?.message || "Terjadi kesalahan";
+
+                    alert(message);
+                }
+            });
+        }
+    }
+    
+    return (
+        <tbody>
+            {pelanggaran.map((data: any) => (
+                <tr>
+                    <td className="border border-gray-300 px-2 py-4 text-sm md:text-base">
+                        {editId === data.id ? (
+                            <Input 
+                                {...register ("pelanggaran")}
+                                defaultValue={data.pelanggaran}
+                                error={errors?.pelanggaran?.message}
+                            />
+                        ) : (
+                            <>{data.pelanggaran}</>
+                        )}
+                    </td>
+                    <td className="border border-gray-300 px-2 py-4 text-sm md:text-base">
+                        <>{data.nomor}</>
+                    </td>
+                    <td className="border border-gray-300 items-center text-center py-4 text-sm md:text-base px-2">
+                        {editId === data.id ? (
+                            <Input 
+                                {...register ("poin")}
+                                defaultValue={data.poin}
+                                error={errors?.poin?.message}
+                            />
+                        ) : (
+                            <>{data.poin}</>
+                        )}
+                    </td>
+                    <td className="border border-gray-300 py-4">
+                        {editId === data.id ? ( isPending ? (
+                            <div className="w-full flex flex-row justify-center items-center">
+                                <FaSpinner
+                                    size={18}
+                                    className="animate-spin text-gray-500"
+                                    type="button"
+                                />
+                            </div>
+                        ) : <div className="w-full flex flex-row justify-center items-center">
+                                <button type="submit">
+                                    <FaSave
+                                        size={18}
+                                        className="cursor-pointer text-green-600"
+                                    />
+                                </button>
+                            </div>
+                        ) : (
+                            <div className="w-full flex flex-row justify-center items-center">
+                                <FaEdit
+                                    size={18}
+                                    className="cursor-pointer text-black"
+                                    type="button"
+                                    onClick={() => {
+                                        setEditId(data.id)
+
+                                        setValue("pelanggaran", data.pelanggaran)
+                                        setValue("poin", String(data.poin))
+                                    }}
+                                />
+                            </div>
+                        )}
+                    </td>
+                    <td className="border border-gray-300 py-4">
+                        <div className="w-full flex flex-row justify-center items-center">
+                            {deleteId === data.id ? ( isPendingDeletePelanggaran ? (
+                                    <div className="w-full flex flex-row justify-center items-center">
+                                        <FaSpinner
+                                            size={18}
+                                            className="animate-spin text-gray-500"
+                                            type="button"
+                                        />
+                                    </div>
+                                ) : ( <div className="w-full flex flex-row justify-center items-center">
+                                        <FaTrashAlt 
+                                            color="red" size={18}
+                                            onClick={
+                                                () => handleDeletePelanggaraan(data.id)
+                                            }
+                                        />
+                                    </div>
+                                )) : (
+                                    <FaTrashAlt 
+                                        color="red" size={18}
+                                        onClick={
+                                            () => handleDeletePelanggaraan(data.id)
+                                        }
+                                    />
+                                )
+                            }
+                        </div>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    )
+}
+
+export default BodyTable;
