e97a2a9e056b2d5ccd3873be77848311e7773538
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-11 18:49:09 +0700

    create variabletype

---
+++ frontend_web/src/types/variable.type.ts
@@ -0,0 +1,15 @@
+export type JenisPelanggaranType = {
+  id: number
+  jenis_pelanggaran: string
+}
+
+export type idJenisPelanggaran = {
+    id: number
+}
+
+export type PelanggaranType = {
+  pelanggaran: {
+    pelanggaran: string;
+    poin: number;
+  }[];
+}
