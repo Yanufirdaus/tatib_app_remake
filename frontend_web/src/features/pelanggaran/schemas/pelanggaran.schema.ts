046b0d49ea75e849a7456f9b79f20071ccd98c52
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:14:25 +0700

    menambah schema untuk add pelanggaran

--- frontend_web/src/features/pelanggaran/schemas/pelanggaran.schema.ts
+++ frontend_web/src/features/pelanggaran/schemas/pelanggaran.schema.ts
@@ -6,3 +6,14 @@ export const UpdatePelanggaranSchema = z.object({
 });
 
 export type UpdatePelanggaranFormValues = z.infer<typeof UpdatePelanggaranSchema>
+
+export const AddPelanggaranSchema = z.object({
+    pelanggaran: z.array(z.object({
+        jenisId: z.string().min(1, { message: "Jenis ID is required" }),
+        pelanggaran: z.string().min(1, { message: "Pelanggaran is required" }),
+        poin: z.string().min(1, { message: "Poin Pelanggaran is required" }),
+        nomor: z.string().min(1, { message: "Nomor Pelanggaran is required" }),
+    }))
+});
+
+export type AddPelanggaranFormValues = z.infer<typeof AddPelanggaranSchema>
