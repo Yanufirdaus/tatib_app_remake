861494c4578719783f2b68da3eac516f80c6acd2
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:25:51 +0700

    create be service for getkelasbyid

--- frontend_web/src/features/kelas/services/kelas.services.ts
+++ frontend_web/src/features/kelas/services/kelas.services.ts
@@ -5,16 +5,25 @@ export const getKelas = async () => {
     try {
         const response = await apiClient.get("/kelas");
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data kelas")
     }
 };
 
-export const deleteKelas = async (id:number) => {
+export const getKelasById = async (id: number) => {
+    try {
+        const response = await apiClient.get(`/kelas/${id}`);
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "gagal mendapatkan data kelas")
+    }
+};
+
+export const deleteKelas = async (id: number) => {
     try {
         const response = await apiClient.delete(`/kelas/${id}`);
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data kelas")
     }
 };
@@ -23,7 +32,7 @@ export const addKelas = async (data: AddKelasFormValues) => {
     try {
         const response = await apiClient.post(`/kelas/batch`, data);
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal menambahkan data kelas")
     }
 }
