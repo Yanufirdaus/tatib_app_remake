45edf029080321949206503b94c97510c564ce23
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:26:50 +0700

    add listkelas for manajemenkelas root

--- frontend_web/src/features/kelas/components/ManajemenKelasRoot.tsx
+++ frontend_web/src/features/kelas/components/ManajemenKelasRoot.tsx
@@ -4,6 +4,7 @@ import { useFieldArray, useForm } from "react-hook-form"
 import { AddManyKelasSchema, type AddKelasFormValues } from "../schema/add.kelas.schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useAddKelas } from "../hooks/useKelas"
+import TitlePage from "../../../components/ui/TitlePage"
 
 const ManajemenKelasRoot = () => {
     const { mutate, isPending, error: addKelasError } = useAddKelas();
@@ -16,7 +17,7 @@ const ManajemenKelasRoot = () => {
         reset
     } = useForm<AddKelasFormValues>({
         resolver: zodResolver(AddManyKelasSchema),
-        defaultValues: { kelas: [] } // array root
+        defaultValues: { kelas: [] }
     });
 
     const { fields, append, remove } = useFieldArray({
@@ -35,7 +36,7 @@ const ManajemenKelasRoot = () => {
 
                 console.log(addKelasError?.message)
             },
-            onError: (error:any) => {
+            onError: (error: any) => {
                 const message = error?.response?.data?.message || error?.message || "Terjadi kesalahan";
                 alert(message);
             }
@@ -44,24 +45,24 @@ const ManajemenKelasRoot = () => {
     }
 
     return (
-         <div className="flex flex-col min-w-screen min-h-screen items-center py-8 gap-6">
-            <div className="flex flex-row min-w-screen">
+        <div className="flex flex-col w-full min-h-screen items-center py-8 gap-6">
+            <div className="flex flex-row w-full">
                 <div className="basis-2/7"></div>
                 <div className="basis-3/7">
-                    <h1 className="text-lg md:text-2xl font-bold text-center">Manajemen Kelas</h1>
+                    <TitlePage title="Manajemen Kelas" />
                 </div>
                 <div className="flex flex-row basis-2/7 justify-center items-center">
-                    <FaPlusSquare 
+                    <FaPlusSquare
                         className="size-6 fill-green-600"
                         onClick={() => {
                             append({
                                 name: "",
                                 grade: "10"
                             })
-                        }}/>
+                        }} />
                 </div>
             </div>
-            <ListKelas fields={fields} register={register} cancelAddHandler={() => { remove() } } onSubmit={handleSubmit(onSubmit)} isPendingAddKelas={isPending} errors={errors}/>
+            <ListKelas fields={fields} register={register} cancelAddHandler={() => { remove() }} onSubmit={handleSubmit(onSubmit)} isPendingAddKelas={isPending} errors={errors} />
         </div>
     )
 }
