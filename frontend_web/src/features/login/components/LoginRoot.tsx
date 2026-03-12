cdc34e7a83fe8623b0a880bf9f9202453b105b2f
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:25:16 +0700

    fix width at loginpage

--- frontend_web/src/features/login/components/LoginRoot.tsx
+++ frontend_web/src/features/login/components/LoginRoot.tsx
@@ -2,7 +2,7 @@ import Loginform from "./LoginForm"
 
 const LoginRoot = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-teal-400 gap-8 py-12">
+        <div className="min-h-screen min-w-screen flex flex-col items-center justify-center bg-teal-400 gap-8 py-12">
             <p className='text-white font-extrabold text-lg md:text-3xl'>LOGIN ADMIN</p>
             <Loginform />
         </div>
