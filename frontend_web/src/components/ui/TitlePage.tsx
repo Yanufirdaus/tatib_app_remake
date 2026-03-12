1589fb3970571c53bdea8b4f9127bcd6fd77839e
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:30:25 +0700

    create titlepage component

---
+++ frontend_web/src/components/ui/TitlePage.tsx
@@ -0,0 +1,7 @@
+const TitlePage = ({ title }: { title: string }) => {
+    return (
+        <h1 className="text-lg md:text-2xl font-bold text-center">{title}</h1>
+    )
+}
+
+export default TitlePage;
