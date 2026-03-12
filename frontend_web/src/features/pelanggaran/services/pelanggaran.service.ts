d9c085d2c57acdf8db15498882b888ba3ce72127
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:14:04 +0700

    menambah service untuk add pelanggaran

--- frontend_web/src/features/pelanggaran/services/pelanggaran.service.ts
+++ frontend_web/src/features/pelanggaran/services/pelanggaran.service.ts
@@ -1,38 +1,47 @@
 import { apiClient } from "../../../services/apiClient";
-import type { UpdatePelanggaranFormValues } from "../schemas/pelanggaran.schema";
+import type { AddPelanggaranFormValues, UpdatePelanggaranFormValues } from "../schemas/pelanggaran.schema";
 
 export const getJenisPelanggaran = async () => {
     try {
         const response = await apiClient.get("/pelanggaran/jenis");
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data jenis pelanggaran")
     }
 };
 
-export const getPelanggaranByJenis = async (id:number) => {
+export const getPelanggaranByJenis = async (id: number) => {
     try {
         const response = await apiClient.get(`/pelanggaran/jenis/${id}`);
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data kelas")
     }
 };
 
-export const updatePelanggaran = async (id:number, data: UpdatePelanggaranFormValues) => {
+export const updatePelanggaran = async (id: number, data: UpdatePelanggaranFormValues) => {
     try {
         const response = await apiClient.put(`/pelanggaran/${id}`, data);
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data semester")
     }
 }
 
-export const deletePelanggaran = async (id:number) => {
+export const deletePelanggaran = async (id: number) => {
     try {
         const response = await apiClient.delete(`/pelanggaran/${id}`);
         return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal mendapatkan data kelas")
     }
 };
+
+export const addPelanggaran = async (data: AddPelanggaranFormValues) => {
+    try {
+        const response = await apiClient.post(`/pelanggaran`, data);
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "gagal menambahkan data kelas")
+    }
+}
