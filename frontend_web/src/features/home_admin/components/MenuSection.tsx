fa4e9b8e83dad25de1d405b37f588def5f2074ae
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:29:47 +0700

    add navigation to manajemen-siswa page

--- frontend_web/src/features/home_admin/components/MenuSection.tsx
+++ frontend_web/src/features/home_admin/components/MenuSection.tsx
@@ -7,28 +7,28 @@ const MenuSection = () => {
 
     const navigate = useNavigate()
     return (
-        <div className="basis-1/5 flex flex-col min-h-12 md:min-h-screen min-w-screen md:min-w-auto items-center justify-center bg-[#537D96] md:bg-[#537D96]">
+        <div className="md:basis-1/5 flex flex-col min-h-12 md:min-h-screen min-w-screen md:min-w-auto items-center justify-center bg-[#537D96] md:bg-[#537D96]">
             <button
                 className="md:hidden text-white py-4"
                 onClick={() => setOpen(!open)}
             >
-                <FaBars/>
+                <FaBars />
             </button>
 
             {open && (
                 <div className="flex flex-col gap-4 md:hidden justify-center items-center bg-gray-800 w-full py-4">
-                    <a className="text-white">Manajemen Siswa</a>
+                    <a className="text-white" onClick={() => { navigate("/manajemen-siswa") }}>Manajemen Siswa</a>
                     <a className="text-white">Manajemen Tendik</a>
-                    <a className="text-white" onClick={()=>{navigate("/kelas")}}>Manajemen Kelas</a>
-                    <a className="text-white" onClick={()=>{navigate("/pelanggaran")}}>Manajemen Peraturan</a>
+                    <a className="text-white" onClick={() => { navigate("/kelas") }}>Manajemen Kelas</a>
+                    <a className="text-white" onClick={() => { navigate("/pelanggaran") }}>Manajemen Peraturan</a>
                 </div>
             )}
 
             <div className="hidden md:flex md:flex-col gap-8 text-white md:w-5/5">
-                <a className="text-white hover:bg-gray-800 md:px-8 py-4">Manajemen Siswa</a>
+                <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate("/manajemen-siswa") }}>Manajemen Siswa</a>
                 <a className="text-white hover:bg-gray-800 md:px-8 py-4">Manajemen Tendik</a>
-                <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={()=>{navigate('/kelas')}}>Manajemen Kelas</a>
-                <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={()=>{navigate("/pelanggaran")}}>Manajemen Peraturan</a>
+                <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate('/kelas') }}>Manajemen Kelas</a>
+                <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate("/pelanggaran") }}>Manajemen Peraturan</a>
             </div>
         </div>
     )
