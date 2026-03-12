598b00e79c9d6080174476f15b47a15412c74629
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:13:02 +0700

    fix width layout pada root layout

--- frontend_web/src/layouts/RootLayout.tsx
+++ frontend_web/src/layouts/RootLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { useAuthStore } from "../store/auth.store";
@@ -9,39 +9,39 @@ const RootLayout = () => {
   const isLoginPage = location.pathname === "/login";
 
   const authChecked = useAuthStore((s) => s.authChecked)
-  
+
   return (
     <div className="min-h-screen w-full bg-gray-100 flex justify-center">
-      
-      <div className="flex flex-col min-h-screen w-full max-w-screen-2xl bg-white">
+
+      <div className="flex flex-col min-h-screen w-full bg-white">
         {!isLoginPage && (
-            <Header />
+          <Header />
         )}
-        
+
         <main className="flex-1">
-            {/* <Outlet /> */}
-            {!authChecked ? (
-              <div className="flex flex-1 h-svh items-center justify-center">
-                  <ThreeDots
-                    visible={true}
-                    height="80"
-                    width="80"
-                    color="#2dd4bf"
-                    radius="9"
-                    ariaLabel="three-dots-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                  />
-              </div>
-            ) : (
-              <div>
-                <Outlet />
-              </div>
-            )}
+          {/* <Outlet /> */}
+          {!authChecked ? (
+            <div className="flex flex-1 h-svh items-center justify-center">
+              <ThreeDots
+                visible={true}
+                height="80"
+                width="80"
+                color="#2dd4bf"
+                radius="9"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+              />
+            </div>
+          ) : (
+            <div>
+              <Outlet />
+            </div>
+          )}
         </main>
-        
+
         {!isLoginPage && (
-            <Footer />
+          <Footer />
         )}
       </div>
 
