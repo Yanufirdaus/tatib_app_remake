fee47f024e858088aed60ddbd661b22ae897005d
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:30:08 +0700

    fix width at adminhome root

--- frontend_web/src/features/home_admin/components/AdminHomeRoot.tsx
+++ frontend_web/src/features/home_admin/components/AdminHomeRoot.tsx
@@ -4,14 +4,13 @@ import MenuSection from "./MenuSection";
 
 const AdminHomeRoot = () => {
     return (
-        <div className="flex flex-col md:flex-row min-h-screen items-center justify-center">
+        <div className="flex flex-col md:flex-row min-h-screen w-full items-center justify-center">
             <MenuSection />
-            <div className="basis-4/5 flex flex-col flex-1 items-center justify-center gap-8">
+            <div className="md:basis-4/5 flex flex-col flex-1 items-center justify-center gap-8">
                 <HeroSection />
                 <SemesterSection />
             </div>
         </div>
-        
     )
 }
 
