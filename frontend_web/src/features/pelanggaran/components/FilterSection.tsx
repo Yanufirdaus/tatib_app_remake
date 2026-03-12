f4aac84a9b2f36b79b1710b9204a9c3ec8aa7f60
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:19:20 +0700

    membuat filterselect components

--- frontend_web/src/features/pelanggaran/components/FilterSection.tsx
+++ frontend_web/src/features/pelanggaran/components/FilterSection.tsx
@@ -1,14 +1,14 @@
 import SelectOption from "../../../components/ui/option"
-import type { FilterSectionProps } from "../../../types/input.types";
+import type { FilterSectionProps } from "../type/pelanggaran.type";
 
 const FilterSection = ({
     isLoading, options, register
-} : FilterSectionProps ) => {
+}: FilterSectionProps) => {
 
     return (
-        <div className="flex flex-row min-w-screen px-4 md:px-0 justify-left items-center md:justify-center">
+        <div className="flex flex-row w-full px-6 md:px-0 justify-left items-center md:justify-center">
             <form>
-                <SelectOption selectOption={isLoading ? [] : options} label="Jenis Pelanggaran :   " {...register("jenisPelanggaran")}/>
+                <SelectOption selectOption={isLoading ? [] : options} label="Jenis Pelanggaran :   " {...register("jenisPelanggaran")} />
             </form>
         </div>
     )
