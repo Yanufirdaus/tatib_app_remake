d1c9ef1e66108b4b3b44a39e7e121821d0f674c4
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:11:10 +0700

    memindah type untuk pelanggaran props ke folder type pelanggaran

--- frontend_web/src/types/input.types.ts
+++ frontend_web/src/types/input.types.ts
@@ -1,7 +1,12 @@
 import React from "react"
-import type { PelanggaranType } from "./variable.type"
-import type { FieldErrors, UseFormRegister, UseFormSetValue } from "react-hook-form"
-import type { UpdatePelanggaranFormValues } from "../features/pelanggaran/schemas/pelanggaran.schema"
+
+export interface DataCardProps {
+  text: string;
+  actionIcon: React.ReactNode;
+  onActionClick?: () => void;
+  isActionDisabled?: boolean;
+  onCardClick?: () => void;
+}
 
 export type InputProps = {
   label?: string
@@ -16,24 +21,3 @@ export type OptionProps = {
   }[]
   label?: string
 } & React.SelectHTMLAttributes<HTMLSelectElement>
-
-export type ListPelanggaranComponentProps = {
-  isLoadingPelanggaran: boolean,
-  pelanggaran: PelanggaranType['pelanggaran']
-}
-
-export type EditPelanggaranComponentProps = {
-  pelanggaran: PelanggaranType['pelanggaran']
-  editId: number | null
-  setEditId: React.Dispatch<React.SetStateAction<number | null>>
-  register: UseFormRegister<any>;
-  isPending:boolean,
-  setValue: UseFormSetValue<UpdatePelanggaranFormValues>
-  errors: FieldErrors<UpdatePelanggaranFormValues>
-} & React.InputHTMLAttributes<HTMLInputElement>
-
-export type FilterSectionProps = {
-  isLoading: boolean;
-  options: OptionProps['selectOption']
-  register: UseFormRegister<any>;
-};
