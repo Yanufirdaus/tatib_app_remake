14a1b0d41c9d9583580555457f6fdff097e6fb50
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:31:59 +0700

    add classname parameter for optionselect components

--- frontend_web/src/components/ui/option.tsx
+++ frontend_web/src/components/ui/option.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import type { OptionProps } from "../../types/input.types";
 
 const SelectOption = React.forwardRef<HTMLSelectElement, OptionProps>(
-  ({ selectOption, label, className = "", ...props }, ref) => {
-    return (
-        <label className="text-xs md:text-base">
-            {label}
-            <select 
-                ref={ref}
-                {...props}
-                className="border p-1 rounded-md focus:outline-none focus:ring-0 min-w-30 md:min-w-50 text-xs md:text-sm"
-            >
-                {selectOption.map((opt) => (
-                    <option key={opt.value} value={opt.value}>
-                        {opt.label}
-                    </option>
-                ))}
-            </select>
-        </label>
-    )
-  }
+    ({ selectOption, label, className = "", ...props }, ref) => {
+        return (
+            <label className="text-xs md:text-base">
+                {label}
+                <select
+                    ref={ref}
+                    {...props}
+                    className={`border p-1 rounded-md focus:outline-none focus:ring-0 text-xs md:text-sm ${className}`}
+                >
+                    {selectOption.map((opt) => (
+                        <option key={opt.value} value={opt.value}>
+                            {opt.label}
+                        </option>
+                    ))}
+                </select>
+            </label>
+        )
+    }
 )
 
 SelectOption.displayName = "selectOption"
