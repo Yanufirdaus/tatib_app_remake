1d7cff48b732975c831afe52dae3791b8d709657
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-12 21:12:37 +0700

    membuat manajemen siswa page

---
+++ frontend_web/src/pages/ManajemenSiswa.tsx
@@ -0,0 +1,9 @@
+import ManajemenSiswaRoot from "../features/manage_user/manage_siswa/components/ManajemenSiswaRoot";
+
+const ManajemenSiswa = () => {
+    return (
+        <ManajemenSiswaRoot />
+    )
+}
+
+export default ManajemenSiswa;
