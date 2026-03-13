062e7e47303538c09ecf62834d69e7d2667a07e1
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-13 22:37:58 +0700

    fix route bug  in the user put route

--- backend/routes/User.ts
+++ backend/routes/User.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { UserController } from '../controllers/UserController';
 import { AdminMiddleware, AuthMidleware, TendikAdminMiddleware } from '../midlewares/AuthMidleware';
-import { UpdateSiswaInputCheckingMiddleware, UpdateTendikInputCheckingMiddleware } from '../midlewares/UserMidleware';
+import { UpdateManySiswaKelasInputCheckingMiddleware, UpdateSiswaInputCheckingMiddleware, UpdateTendikInputCheckingMiddleware } from '../midlewares/UserMidleware';
 
 const userRouter = express.Router();
 
@@ -12,6 +12,8 @@ userRouter.get("/tendik/:id", AuthMidleware, UserController.getTendikById);
 userRouter.get("/tendik/role/:role", AuthMidleware, AdminMiddleware, UserController.getTendikByRole);
 
 userRouter.get("/kelas/:kelasId/students", AuthMidleware, TendikAdminMiddleware, UserController.getSiswaByKelas);
+
+userRouter.put("/students/kelas", AuthMidleware, AdminMiddleware, UpdateManySiswaKelasInputCheckingMiddleware, UserController.updateManySiswaKelas);
 
 userRouter.put("/students/:id", AuthMidleware, AdminMiddleware, UpdateSiswaInputCheckingMiddleware, UserController.updateSiswa);
 
