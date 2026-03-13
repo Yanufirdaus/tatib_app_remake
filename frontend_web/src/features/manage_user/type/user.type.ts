bf4e70cd118059698497cb596a18e272a376e8d6
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-13 22:27:48 +0700

    create props for siswa list and siswa rows

---
+++ frontend_web/src/features/manage_user/type/user.type.ts
@@ -0,0 +1,13 @@
+import type { Control, FieldArrayWithId } from "react-hook-form";
+import type { UpdateManySiswaKelasFormValues } from "../schema/user.schema";
+
+export type SiswaRowProps = {
+    s: any;
+    editId: number | null;
+    setEditId: (id: number | null) => void;
+    isLoadingKelas: boolean;
+    options: { value: string, label: string }[];
+    fields: FieldArrayWithId<UpdateManySiswaKelasFormValues, "kelasUpdate">[];
+    controlKelas: Control<UpdateManySiswaKelasFormValues>;
+    index: number;
+}
