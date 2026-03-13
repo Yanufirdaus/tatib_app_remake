9803558ebcf1d4902155d5f40c45bbc54f24175b
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-13 22:30:01 +0700

    create constant variable for siswa table

---
+++ frontend_web/src/features/manage_user/manage_siswa/constants/table.ts
@@ -0,0 +1,3 @@
+export const HEADERS = ["Nama", "NISN", "Kelas", "Poin", "Edit"];
+export const thClass = "border border-gray-300 py-2 px-2 whitespace-nowrap";
+export const tdClass = "border border-gray-300 px-2 py-4 text-sm md:text-base whitespace-nowrap";
