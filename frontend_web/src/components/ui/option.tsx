4f34dc5a9d3d579e425200e29a189ce4c3a28031
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-13 22:33:37 +0700

    add placehoder on selectoption

--- frontend_web/src/components/ui/option.tsx
+++ frontend_web/src/components/ui/option.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import type { OptionProps } from "../../types/input.types";
 
 const SelectOption = React.forwardRef<HTMLSelectElement, OptionProps>(
-    ({ selectOption, label, className = "", ...props }, ref) => {
+    ({ selectOption, label, placeholder, className = "", ...props }, ref) => {
         return (
-            <label className="text-xs md:text-base">
+            <label className="text-xs md:text-base w-full">
                 {label}
                 <select
                     ref={ref}
                     {...props}
                     className={`border p-1 rounded-md focus:outline-none focus:ring-0 text-xs md:text-sm ${className}`}
                 >
+                    {placeholder && (
+                        <option value="" disabled hidden>
+                            {placeholder}
+                        </option>
+                    )}
                     {selectOption.map((opt) => (
                         <option key={opt.value} value={opt.value}>
                             {opt.label}
