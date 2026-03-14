21e88523b6dc0ac30a19132261518fb74c599a6b
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-14 22:14:22 +0700

    add tambahbanyaksiswa components

--- frontend_web/src/features/manage_user/manage_siswa/components/ManajemenSiswaRoot.tsx
+++ frontend_web/src/features/manage_user/manage_siswa/components/ManajemenSiswaRoot.tsx
@@ -1,10 +1,12 @@
 import TitlePage from "../../../../components/ui/TitlePage";
 import ListKelas from "./ListKelas";
+import TambahBanyakSiswa from "./TambahBanyakSiswa";
 
 const ManajemenSiswaRoot = () => {
     return (
-        <div className="flex flex-col min-h-screen w-full py-6 md:py-8">
+        <div className="flex flex-col min-h-screen w-full gap-4 items-center py-6 md:py-8">
             <TitlePage title="Manajemen Siswa" />
+            <TambahBanyakSiswa />
             <ListKelas />
         </div>
     )
