c6a81d79816ab08414faf3804039b50523b5046b
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-15 21:13:21 +0700

    create page for list role of tendik in tendik management

---
+++ frontend_web/src/features/manage_user/manage_tendik/components/ListRoleTendik.tsx
@@ -0,0 +1,29 @@
+import { FaChevronRight } from "react-icons/fa";
+import DataCard from "../../../../components/ui/DataCard";
+import { useNavigate } from "react-router-dom";
+
+const ListRoleTendik = () => {
+    const navigate = useNavigate();
+    const listRoleTendik = [
+        { id: 1, role: "kesiswaan" },
+        { id: 2, role: "bk" },
+        { id: 3, role: "kepsek" }
+    ]
+
+    return (
+        <div className="flex flex-col gap-2 mt-4 w-full md:w-auto items-center">
+            <div className="flex flex-col gap-2">
+                {listRoleTendik.map((item: any) => (
+                    <DataCard
+                        key={item.id}
+                        text={item.role}
+                        actionIcon={<FaChevronRight className="fill-gray-600 transition-colors" />}
+                        onCardClick={() => navigate(`/manajemen-tendik/role/${item.role}`)}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ListRoleTendik;
