1581e4f7701186d734f0ac64e642f27be09e4f07
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-15 21:11:45 +0700

    create hook for manajemen tendik

---
+++ frontend_web/src/features/manage_user/manage_tendik/hooks/useTendik.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteTendik, getTendikByRole, updateTendik } from "../services/tendik.service";
+
+export const useGetTendikByRole = (role: string) => {
+    return useQuery({
+        queryKey: ["tendik", role],
+        queryFn: () => getTendikByRole(role),
+        staleTime: 5 * 60 * 1000
+    })
+}
+
+export const useUpdateTendik = (id: number) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (data: any) => updateTendik(id, data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["tendik"] });
+        }
+    })
+}
+
+export const useDeleteTendik = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (id: number) => deleteTendik(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["tendik"] });
+        }
+    })
+}
