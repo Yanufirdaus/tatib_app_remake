c634031e72ea394990bccf66d4ed89bce8194896
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-15 21:14:30 +0700

    add route to go to majamemen tendik page

--- frontend_web/src/features/home_admin/components/MenuSection.tsx
+++ frontend_web/src/features/home_admin/components/MenuSection.tsx
@@ -18,7 +18,7 @@ const MenuSection = () => {
             {open && (
                 <div className="flex flex-col gap-4 md:hidden justify-center items-center bg-gray-800 w-full py-4">
                     <a className="text-white" onClick={() => { navigate("/manajemen-siswa") }}>Manajemen Siswa</a>
-                    <a className="text-white">Manajemen Tendik</a>
+                    <a className="text-white" onClick={() => { navigate("/manajemen-tendik") }}>Manajemen Tendik</a>
                     <a className="text-white" onClick={() => { navigate("/kelas") }}>Manajemen Kelas</a>
                     <a className="text-white" onClick={() => { navigate("/pelanggaran") }}>Manajemen Peraturan</a>
                 </div>
@@ -26,7 +26,7 @@ const MenuSection = () => {
 
             <div className="hidden md:flex md:flex-col gap-8 text-white md:w-5/5">
                 <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate("/manajemen-siswa") }}>Manajemen Siswa</a>
-                <a className="text-white hover:bg-gray-800 md:px-8 py-4">Manajemen Tendik</a>
+                <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate("/manajemen-tendik") }}>Manajemen Tendik</a>
                 <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate('/kelas') }}>Manajemen Kelas</a>
                 <a className="text-white hover:bg-gray-800 md:px-8 py-4" onClick={() => { navigate("/pelanggaran") }}>Manajemen Peraturan</a>
             </div>
