39eccd424a98a5f3af9835effcd3d954aaffca58
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-17 11:56:13 +0700

    create service for add tendik

--- frontend_web/src/features/manage_user/manage_tendik/services/tendik.service.ts
+++ frontend_web/src/features/manage_user/manage_tendik/services/tendik.service.ts
@@ -1,4 +1,5 @@
 import { apiClient } from "../../../../services/apiClient";
+import type { CreateTendikFormValues, UpdateTendikFormValues } from "../../schema/user.schema";
 
 export const getTendikByRole = async (role: string) => {
     try {
@@ -18,11 +19,28 @@ export const deleteTendik = async (id: number) => {
     }
 }
 
-export const updateTendik = async (id: number, data: any) => {
+export const updateTendik = async (id: number, data: UpdateTendikFormValues) => {
     try {
         const response = await apiClient.put(`/tendik/${id}`, data);
         return response.data;
     } catch (error: any) {
         throw new Error(error.response?.data?.message || "gagal memperbarui data tendik")
+    }
+}
+
+export const addTendik = async (data: CreateTendikFormValues) => {
+    try {
+        const payload = {
+            tendik: data.tendik.map((t) => ({
+                ...t,
+                image_profile: "https://res.cloudinary.com/drw93apjn/image/upload/v1773338893/profileawal_ossrac.jpg",
+                password: t.nip,
+            }))
+        }
+
+        const response = await apiClient.post(`/register/tendik`, payload);
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "gagal menambahkan data tendik")
     }
 }
