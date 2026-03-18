307a80ffaf568e90e15ea68a17ea684f31fce504
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 16:51:58 +0700

    create type model for all required data

---
+++ frontend_web/src/types/models.ts
@@ -0,0 +1,51 @@
+import type { TendikRole } from "@/constants/roles";
+
+export interface Profile {
+  id: number;
+  name: string;
+  role: TendikRole;
+  image_profile?: string;
+}
+
+export type User = Profile;
+
+export interface Siswa {
+  id: number;
+  nisn: string;
+  poin: number;
+  kelasId: number;
+  profileSiswa: Profile;
+  kelas: Kelas;
+}
+
+export interface Tendik {
+  id: number;
+  nip: string;
+  profileSiswa: Profile;
+}
+
+export interface Kelas {
+  id: number;
+  name: string;
+  grade: string;
+}
+
+export interface Semester {
+  id: number;
+  semester: 'ganjil' | 'genap';
+  tahun_ajaran: string;
+  is_active: boolean;
+}
+
+export interface JenisPelanggaran {
+  id: number;
+  jenis_pelanggaran: string;
+}
+
+export interface Pelanggaran {
+  id: number;
+  nomor: string;
+  pelanggaran: string;
+  poin: number;
+  jenisId: number;
+}
