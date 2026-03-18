8a5a4e23c9df416ca01be11c661eb36c95fa73b9
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 19:19:43 +0700

    refactor siswakelasroot

--- frontend_web/src/features/manage_user/manage_siswa/components/siswa_kelas/SiswaKelasRoot.tsx
+++ frontend_web/src/features/manage_user/manage_siswa/components/siswa_kelas/SiswaKelasRoot.tsx
@@ -1,9 +1,9 @@
-import { FaPlusSquare } from "react-icons/fa";
-import TitlePage from "../../../../../components/ui/TitlePage";
 import ListSiswa from "./ListSiswa";
 import AddSiswa from "./AddSiswa";
-import ActionButtons from "../../../../../components/ui/ActionButtons";
-import { useSiswaKelasRoot } from "../../hooks/useSiswaKelasRoot";
+import ActionButtons from "@/components/ui/ActionButtons";
+import { useSiswaKelasRoot } from "@/features/manage_user/manage_siswa/hooks/useSiswaKelasRoot";
+import PageContainer from "@/components/ui/PageContainer";
+import FeatureHeader from "@/components/ui/FeatureHeader";
 
 const SiswaKelasRoot = ({ id }: { id: number }) => {
     const {
@@ -23,25 +23,12 @@ const SiswaKelasRoot = ({ id }: { id: number }) => {
     } = useSiswaKelasRoot(id);
 
     return (
-        <div className="flex flex-col min-h-screen w-full py-6 md:py-8 gap-8">
-            <div className="flex flex-row w-full">
-                <div className="basis-2/8"></div>
-                <div className="basis-6/8">
-                    <TitlePage title={`Siswa Kelas ${isLoadingKelas ? "" : kelas?.name.toUpperCase()}`} />
-                </div>
-                <div className="basis-2/8">
-                    {
-                        isAddSiswa ? (
-                            <></>
-                        ) : (
-                            <FaPlusSquare
-                                className="size-6 fill-green-600 cursor-pointer"
-                                onClick={() => { setIsAddSiswa(true) }}
-                            />
-                        )
-                    }
-                </div>
-            </div>
+        <PageContainer className="py-6 px-6">
+            <FeatureHeader
+                title={`Siswa Kelas ${isLoadingKelas ? "" : kelas?.name.toUpperCase()}`}
+                onActionClick={() => setIsAddSiswa(true)}
+                showAction={!isAddSiswa}
+            />
             <div className="flex justify-center">
                 {
                     isEditKelasSiswa ? (
@@ -69,8 +56,8 @@ const SiswaKelasRoot = ({ id }: { id: number }) => {
                     <AddSiswa setIsAddSiswa={setIsAddSiswa} kelasId={id} />
                 )
             }
-            <ListSiswa siswa={siswa} isLoadingSiswa={isLoadingSiswa} fields={fields} controlKelas={control} />
-        </div>
+            <ListSiswa siswa={siswa || []} isLoadingSiswa={isLoadingSiswa} fields={fields} controlKelas={control} />
+        </PageContainer>
     )
 }
 
