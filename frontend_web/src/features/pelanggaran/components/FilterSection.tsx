0a202bf63b6491b3691a436fd37f463d472d2b01
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 17:27:25 +0700

    refafctor filtersection

--- frontend_web/src/features/pelanggaran/components/FilterSection.tsx
+++ frontend_web/src/features/pelanggaran/components/FilterSection.tsx
@@ -1,5 +1,5 @@
-import SelectOption from "../../../components/ui/option"
-import type { FilterSectionProps } from "../type/pelanggaran.type";
+import SelectOption from "@/components/ui/SelectOption"
+import type { FilterSectionProps } from "@/features/pelanggaran/type/pelanggaran.type";
 
 const FilterSection = ({
     isLoading, options, register
@@ -8,7 +8,11 @@ const FilterSection = ({
     return (
         <div className="flex flex-row w-full px-6 md:px-0 justify-left items-center md:justify-center">
             <form>
-                <SelectOption selectOption={isLoading ? [] : options} label="Jenis Pelanggaran :   " {...register("jenisPelanggaran")} />
+                <SelectOption 
+                    selectOption={isLoading ? [] : options} 
+                    label="Jenis Pelanggaran :   " 
+                    {...register("jenisPelanggaran")} 
+                />
             </form>
         </div>
     )
