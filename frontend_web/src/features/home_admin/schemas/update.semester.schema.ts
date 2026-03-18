7d3f5bfe872dec14c6f84726f220ae1253b9ca64
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 19:28:22 +0700

    fix update semesterschema

---
+++ frontend_web/src/features/home_admin/schemas/update.semester.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const UpdateSemesterSchema = z.object({
+    semester: z.enum(["ganjil", "genap"], { message: "Semester must be either 'ganjil' or 'genap'" }),
+    tahun_ajaran: z.string()
+                .min(1, { message: "Tahun Ajaran is required" })
+                .regex(/^\d{4}-\d{4}$/, {
+                    message: "Format Tahun Ajaran harus seperti 2024-2025"
+                })
+});
+
+export type UpdateSemesterFormValues = z.infer<typeof UpdateSemesterSchema>;
