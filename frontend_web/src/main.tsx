aaff72c4f6ab7dbc96ada64754d58332f2ac1fad
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 20:34:40 +0700

    put errorboundary in the main project

--- frontend_web/src/main.tsx
+++ frontend_web/src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.tsx";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AuthProvider from './app/providers/AuthProvider.tsx';
+import ErrorBoundary from './components/ui/ErrorBoundary.tsx';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +22,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <ErrorBoundary>
+          <RouterProvider router={router} />
+        </ErrorBoundary>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
