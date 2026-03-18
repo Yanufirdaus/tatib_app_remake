3e35e6df37385695cbee524abc3d75dfe47f3aea
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 21:16:31 +0700

    give padding vertical at div cover herosection and semestersection

--- frontend_web/src/features/home_admin/components/AdminHomeRoot.tsx
+++ frontend_web/src/features/home_admin/components/AdminHomeRoot.tsx
@@ -7,7 +7,7 @@ const AdminHomeRoot = () => {
     return (
         <PageContainer className="md:flex-row md:justify-center">
             <MenuSection />
-            <div className="md:basis-4/5 flex flex-col flex-1 items-center justify-center gap-8 px-6">
+            <div className="md:basis-4/5 flex flex-col flex-1 items-center justify-center gap-8 px-6 py-6">
                 <HeroSection />
                 <SemesterSection />
             </div>
