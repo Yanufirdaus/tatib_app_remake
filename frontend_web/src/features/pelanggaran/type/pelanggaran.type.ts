56381300d551510d02fab307048255c333aff1c6
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 17:17:11 +0700

    edit type for pelanggaran input

--- frontend_web/src/features/pelanggaran/type/pelanggaran.type.ts
+++ frontend_web/src/features/pelanggaran/type/pelanggaran.type.ts
@@ -1,8 +1,12 @@
-import type { UpdatePelanggaranFormValues } from "../schemas/pelanggaran.schema"
-import type { PelanggaranType } from "../../../types/variable.type"
+import type { UpdatePelanggaranFormValues } from "@/features/pelanggaran/schemas/pelanggaran.schema";
+import type { Pelanggaran } from "@/types/models"
 import type { FieldArrayWithId, FieldErrors, UseFieldArrayRemove, UseFormHandleSubmit, UseFormRegister, UseFormReset, UseFormSetValue } from "react-hook-form"
-import type { OptionProps } from "../../../types/input.types"
-import type { AddPelanggaranFormValues } from "../schemas/pelanggaran.schema"
+import type { OptionProps } from "@/types/input.types"
+import type { AddPelanggaranFormValues } from "@/features/pelanggaran/schemas/pelanggaran.schema";
+
+export type PelanggaranFilterForm = {
+  jenisPelanggaran: string;
+}
 
 export type UpdatePelanggaranPayload = {
   id: number
@@ -11,23 +15,25 @@ export type UpdatePelanggaranPayload = {
 
 export type ListPelanggaranComponentProps = {
   isLoadingPelanggaran: boolean,
-  pelanggaran: PelanggaranType['pelanggaran']
+  pelanggaran: Pelanggaran[]
 }
 
 export type EditPelanggaranComponentProps = {
-  pelanggaran: PelanggaranType['pelanggaran']
+  pelanggaran: Pelanggaran[]
   editId: number | null
   setEditId: React.Dispatch<React.SetStateAction<number | null>>
-  register: UseFormRegister<any>;
+  register: UseFormRegister<UpdatePelanggaranFormValues>;
   isPending: boolean,
   setValue: UseFormSetValue<UpdatePelanggaranFormValues>
   errors: FieldErrors<UpdatePelanggaranFormValues>
-} & React.InputHTMLAttributes<HTMLInputElement>
+  handleSubmit: UseFormHandleSubmit<UpdatePelanggaranFormValues>
+  onSubmit: (data: UpdatePelanggaranFormValues) => void
+}
 
 export type FilterSectionProps = {
   isLoading: boolean;
   options: OptionProps['selectOption']
-  register: UseFormRegister<any>;
+  register: UseFormRegister<PelanggaranFilterForm>;
 };
 
 export type TambahPelanggaranProps = {
@@ -35,7 +41,7 @@ export type TambahPelanggaranProps = {
   reset: UseFormReset<AddPelanggaranFormValues>
   fields: FieldArrayWithId<AddPelanggaranFormValues, "pelanggaran">[]
   remove: UseFieldArrayRemove
-  register: UseFormRegister<any>
+  register: UseFormRegister<AddPelanggaranFormValues>
   handleSubmit: UseFormHandleSubmit<AddPelanggaranFormValues>
   errors: FieldErrors<AddPelanggaranFormValues>
 }
