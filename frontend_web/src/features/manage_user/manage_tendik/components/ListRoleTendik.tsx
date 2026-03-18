d9fbd4f581bd0095deee39a282bb7b616d64f9ba
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 18:53:43 +0700

    refactor rolelisttendik

--- frontend_web/src/features/manage_user/manage_tendik/components/ListRoleTendik.tsx
+++ frontend_web/src/features/manage_user/manage_tendik/components/ListRoleTendik.tsx
@@ -1,10 +1,10 @@
 import { FaChevronRight } from "react-icons/fa";
-import DataCard from "../../../../components/ui/DataCard";
+import DataCard from "@/components/ui/DataCard";
 import { useNavigate } from "react-router-dom";
 
 const ListRoleTendik = () => {
     const navigate = useNavigate();
-    const listRoleTendik = [
+    const listRoleTendik: { id: number, role: string }[] = [
         { id: 1, role: "kesiswaan" },
         { id: 2, role: "bk" },
         { id: 3, role: "kepsek" }
@@ -13,7 +13,7 @@ const ListRoleTendik = () => {
     return (
         <div className="flex flex-col gap-2 mt-4 w-full md:w-auto items-center">
             <div className="flex flex-col gap-2">
-                {listRoleTendik.map((item: any) => (
+                {listRoleTendik.map((item) => (
                     <DataCard
                         key={item.id}
                         text={item.role}
