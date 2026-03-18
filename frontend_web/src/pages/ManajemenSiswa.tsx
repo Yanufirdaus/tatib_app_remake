52075451dd4c92333de1f99f1d192089be5c216f
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 18:09:41 +0700

    delete or move some file into other directory

--- frontend_web/src/pages/ManajemenSiswa.tsx
+++
@@ -1,9 +0,0 @@
-import ManajemenSiswaRoot from "../features/manage_user/manage_siswa/components/ManajemenSiswaRoot";
-
-const ManajemenSiswa = () => {
-    return (
-        <ManajemenSiswaRoot />
-    )
-}
-
-export default ManajemenSiswa;
--- frontend_web/src/pages/ManajemenTendik.tsx
+++
@@ -1,9 +0,0 @@
-import ManajemenTendikRoot from "../features/manage_user/manage_tendik/components/ManajemenTendikRoot";
-
-const ManajemenTendik = () => {
-    return (
-        <ManajemenTendikRoot />
-    );
-};
-
-export default ManajemenTendik;
