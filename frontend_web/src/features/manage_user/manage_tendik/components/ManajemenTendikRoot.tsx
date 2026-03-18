86d3e3cfee4e158b330b2a3f29494036ac6c4c04
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 18:41:11 +0700

    refactor manajementendikroot page

--- frontend_web/src/features/manage_user/manage_tendik/components/ManajemenTendikRoot.tsx
+++ frontend_web/src/features/manage_user/manage_tendik/components/ManajemenTendikRoot.tsx
@@ -1,12 +1,13 @@
-import TitlePage from "../../../../components/ui/TitlePage";
 import ListRoleTendik from "./ListRoleTendik";
+import PageContainer from "@/components/ui/PageContainer";
+import FeatureHeader from "@/components/ui/FeatureHeader";
 
 const ManajemenTendikRoot = () => {
     return (
-        <div className="flex flex-col min-h-screen w-full gap-4 items-center py-6 md:py-8">
-            <TitlePage title="Manajemen Tendik" />
+        <PageContainer className="py-6">
+            <FeatureHeader title="Manajemen Tendik" showAction={false} />
             <ListRoleTendik />
-        </div>
+        </PageContainer>
     )
 }
 
