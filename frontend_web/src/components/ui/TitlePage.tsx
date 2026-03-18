4caf350ee6c87cd0c966f2167269ed2b7228a435
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-18 19:50:52 +0700

    create component for titlepage

--- frontend_web/src/components/ui/TitlePage.tsx
+++ frontend_web/src/components/ui/TitlePage.tsx
@@ -1,6 +1,6 @@
 const TitlePage = ({ title }: { title: string }) => {
     return (
-        <h1 className="text-lg md:text-2xl font-bold text-center">{title}</h1>
+        <h1 className="text-base md:text-2xl font-bold text-center">{title}</h1>
     )
 }
 
