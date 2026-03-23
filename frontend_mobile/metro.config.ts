66a8c9935e96a38e7a98639419f792bb1eee6d60
Firdaus Putra Maulidyanu <yanufirdaus@gmail.com>
2026-03-23 23:07:03 +0700

    delete frontend_mobile starter project

--- frontend_mobile/metro.config.ts
+++
@@ -1,8 +0,0 @@
-import { getDefaultConfig } from "expo/metro-config";
-// @ts-ignore
-import { withNativeWind } from "nativewind/metro";
-
-const config = getDefaultConfig(__dirname);
-
-// @ts-ignore
-module.exports = withNativeWind(config, { input: "./global.css" });
